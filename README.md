a34f0b1b260045e061fa9ba5c98991abe5ad30c4
wangjiping <wangjiping@myhome.163.com>
2015-11-24 14:09:28 +0800

    爬取豆瓣电影top250相关信息

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 个人练习nodejs爬虫
+
+1. 抓取豆瓣TOP250的电影及相关参数
+
+2. 使用superagent发送请求
+
+3. 使用aysnc控制最大并发数量
+
+4. 使用cheerio过滤内容
--- crawler.js
+++ crawler.js
@@ -3,22 +3,22 @@ var fs = require("fs");
 var superagent = require('superagent');
 var cheerio = require("cheerio");
 var async = require('async'); //可控制并发数量
-var iconv = require('iconv-lite');
+// var iconv = require('iconv-lite');//字符编码转换
+var eventproxy = require('eventproxy');
+// var ep = new eventproxy();
+var contents = [];
+var filename = 'contents.json';
 var urls = [];
 var concurrencyCount = 0;
-// var eventproxy = require('eventproxy');
-// var ep = new eventproxy();
-var results = [];
-for (var index = 100001, max = 100100; index <= max; index++) {
-    urls.push('http://item.jd.com/' + index + '.html');
+var concurrencyCount = 0;
+for (var index = 0, max = 10; index < max; index++) {
+    urls.push('http://movie.douban.com/top250?start=' + 25 * index);
 }
-async.mapLimit(urls, 10, function(url, callback) {
+async.mapLimit(urls, 4, function(url, callback) {
     concurrencyCount++;
-    // console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url);
+    console.log('现在的并发数是', concurrencyCount, ',正在抓取的是', url);
     superagent.get(url).end(function(err, res) {
-        var html = iconv.decode(res.text || '', 'gbk');
-        var goods = filterGoods(url, html);
-        results.push(goods);
+        filterContents(res && res.text || '');
         concurrencyCount--;
         callback();
     });
@@ -26,28 +26,54 @@ async.mapLimit(urls, 10, function(url, callback) {
     if (err) {
         throw err;
     }
-    console.log('done!');
     writeToFile();
 });
 
 function writeToFile() {
-    var content = [];
-    results.forEach(function(item, index) {
-        content.push(item.index + ' url:' + item.url + '#name:' + item.name + '\n');
+    contents.forEach(function(item, index) {
+        item.index = index + 1;
     });
-    fs.appendFile('jd_good_name.txt', content.join(''), function(err) {
+    fs.appendFile(filename, JSON.stringify(contents), function(err) {
         if (err) {
             throw err;
         }
+        console.log('done!');
     });
 }
 
-function filterGoods(url, html) {
+function filterContents(html) {
+    if (!html) {
+        return;
+    }
     var $ = cheerio.load(html);
-    var name = $('#name').find('h1').text();
-    return {
-        'url': url,
-        'index': +url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')),
-        'name': name
-    };
+    var hd = $('.hd'),
+        bd = $('.bd');
+    for (var i = 0, len = hd.length; i < len; i++) {
+        var select = $(hd[i]).find('a');
+        var url = select.attr('href'),
+            cName = $(select.find('span')[0]).text().trim(),
+            eName = $(select.find('span')[1]).text().trim(),
+            other = $(select.find('span')[2]).text().trim(),
+            playable = $(hd[i]).find('.playable').text().trim(),
+            person = $($(bd[i]).find('p')[0]).text().trim(),
+            ratingNum = $(bd[i]).find('.star').find('.rating_num').text().trim(),
+            ratingStar = ($($(bd[i]).find('.star').find('span')[0]).attr('class') || '').replace(/\D/g, ''),
+            remarkNum = ($($(bd[i]).find('.star').find('span')[3]).text() || '').replace(/\D/g, '');
+        if (ratingStar > 10) {
+            ratingStar = (ratingStar / 10).toFixed(1);
+        } else {
+            ratingStar = (+ratingStar).toFixed(1);
+        }
+        contents.push({
+            'url': url,
+            'cName': cName,
+            'eName': eName,
+            'other': other,
+            'playable': playable,
+            'person': person,
+            'ratingNum': ratingNum,
+            'ratingStar': ratingStar,
+            'remarkNum': remarkNum
+        });
+    }
 }
